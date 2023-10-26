f9acd7567e3704e7479d803d3cd8b9ffeb09dfb7
DylanQuenon <dylan.quenon.04@gmail.com>
2023-10-26 13:34:18 +0200

    connexion bdd

---
+++ src/Controller/AccountController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class AccountController extends AbstractController
+{
+    /**
+     * Permet de se connecter
+     *
+     * @return Response
+     */
+    #[Route('/login', name: 'account_login')]
+    public function index(AuthenticationUtils $utils): Response
+    {
+        $error = $utils->getLastAuthenticationError();
+        $username = $utils->getLastUsername();
+
+
+        return $this->render('account/index.html.twig', [
+            
+            'hasError' => $error !== null,
+            'username' => $username
+        ]);
+    }
+    /**
+     * Permet de se déconnecter
+     *
+     * @return void
+     */
+    #[Route("/logout", name: "account_logout")]
+    public function logout(): void
+    {
+
+    }
+}
